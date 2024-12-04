5e3be4c5b7932b7a89579ed93fd9cd898b6bcf30
Sweta Sergeeva <sweta.sergeeva@gmail.com>
2024-12-04 15:46:19 +0300

    готовое дз2

--- myzero/appdz1/views.py
+++ myzero/appdz1/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Это мой первый проект на Django</h1>")
+    return render(request, 'allapp/index1.html')
 
 
 def test(request):
